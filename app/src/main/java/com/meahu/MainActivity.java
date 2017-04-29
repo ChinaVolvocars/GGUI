a04b1368918c4baf592f9e44c33cbd6023b5d73d
Tiger <atlantisspeed@gmail.com>
2017-04-29 16:32:38 +0800

    <!--4-10 修改支付密码--> <!--4-11、重置支付密码-信息验证--> <!--4-12、重置支付密码-密码重置-->

---
+++ app/src/main/java/com/meahu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.meahu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //6位密码输入框 https://github.com/Jungerr/GridPasswordView
+
+        //使用方法见Readme 文件
+    }
+}
